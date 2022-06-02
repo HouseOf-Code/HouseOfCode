18b697dcf9564cda83914dc5e91e85762ae8dfd9
smselnes <smselnes@gmail.com>
2022-06-02 12:59:03 +0200

    Styling updates

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,10 @@
-console.log("We are the House of Code agency");
+const nav = document.querySelector(".nav__inner");
+const menu = document.querySelector(".nav__menu");
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
+
+let transformIcon = function (icon) {
+  icon.classList.toggle("fa-times");
+};
